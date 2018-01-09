e72adcf8069123b54a61b9552d2a8f823e8e2a98
eylay <ali.seyfi68@gmail.com>
2018-01-09 13:46:02 +0330

    welcome ghelper fixed

--- app/Http/welcome_helpers.php
+++ app/Http/welcome_helpers.php
@@ -24,23 +24,3 @@ function prepare_multiple($inputs)
 
     return $result;
 }
-
-function phone_number($mobile)
-{
-    return substr($mobile,0,4) . ' ' . substr($mobile,4,-4) . ' ' . substr($mobile,7,11);
-}
-
-function toman($value)
-{
-    $value = is_numeric($value) ? number_format($value) : $value;
-    $toman = $value ? $value.' تومان' : $value;
-    return $toman;
-}
-
-function storage($path)
-{
-    $path = 'storage/'.$path;
-    $path = asset($path);
-    $path = str_replace('public/','',$path);
-    return $path;
-}

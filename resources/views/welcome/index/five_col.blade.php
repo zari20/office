37c06567b00a40fd4ffc6cce106accadaafd9488
eylay <ali.seyfi68@gmail.com>
2018-01-17 13:34:48 +0330

    five cols carouselable

--- resources/views/welcome/index/five_col.blade.php
+++ resources/views/welcome/index/five_col.blade.php
@@ -1,15 +1,34 @@
+@php
+    $five_cols = $section->fragments();
+@endphp
+
 <section class="boxes">
     <div class="container">
-        <div class="row">
-            @foreach ($section->fragments() as $key => $col)
-                <div class="box text-center">
-                    <a href="{{$col->href}}">
-                        <i class="fa fa-{{$col->icon}}" aria-hidden="true"></i>
-                        <h3> {{$col->subject}} </h3>
-                        <p> {{$col->info}} </p>
-                    </a>
+        <div id="five-cols-{{$section->id}}" class="carousel slide" data-ride="carousel" data-interval="false">
+            @if (count($five_cols) > 5)
+                <ol class="carousel-indicators carousel-indicators-numbers">
+                    @for ($i=0; $i <= carousel_indicators($five_cols,5); $i++)
+                        <li data-target="#five-cols-{{$section->id}}" data-slide-to="{{$i}}" {!!$i==0 ? 'class="active"' : ''!!}>{{$i+1}}</li>
+                    @endfor
+                </ol>
+            @endif
+            @if (count($five_cols) > 5) <div class="carousel-inner"> @endif
+            @for ($i=0; $i <= carousel_indicators($five_cols,5); $i++)
+                <div class="carousel-item {!! $i==0 ? 'active' : '' !!}">
+                    <div class="row">
+                        @for ($j=($i*5); $j < ($i*5)+5 && $j < count($five_cols) ; $j++)
+                            <div class="box text-center">
+                                <a href="{{$five_cols[$j]->href}}">
+                                    <i class="fa fa-{{$five_cols[$j]->icon}}" aria-hidden="true"></i>
+                                    <h3> {{$five_cols[$j]->subject}} </h3>
+                                    <p> {{$five_cols[$j]->info}} </p>
+                                </a>
+                            </div>
+                        @endfor
+                    </div>
                 </div>
-            @endforeach
+            @endfor
+            @if (count($five_cols) > 5) </div> @endif
         </div>
     </div>
 </section>

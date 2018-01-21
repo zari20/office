bf1db0f0bb6b2bf070a6237fb10299504425d863
eylay <ali.seyfi68@gmail.com>
2018-01-21 15:39:36 +0330

    sticky top menu

--- resources/views/welcome_partials/menu.blade.php
+++ resources/views/welcome_partials/menu.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg" id="top-menu">
+<nav class="navbar navbar-expand-lg sticky-top bg-white" id="top-menu">
     <div class="container">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fa fa-bars"></i>

e6c39ed7f6637861e7100421da36c07433149fcd
eylay <ali.seyfi68@gmail.com>
2018-03-08 12:12:15 +0330

    replace static footer with dynamic footer

--- resources/views/welcome_partials/footer.blade.php
+++ resources/views/welcome_partials/footer.blade.php
@@ -1,211 +1,28 @@
-<footer class="bg-first mt-5">
-    <div class="text-center d-sm-none">
-
-        <div class="text-center mb-4">
-            <img src="http://127.0.0.1:8000/img/icons/royan.png" width="60px" height="60px" alt="رویان رسانه">
-            <p class="royan-desc text-black"> اولین موسس باشگاه مشتریان هوشمند در ایران </p>
-            <a href="http://www.rqmedia.ir" class="d-block royan-desc text-white"> w w w . m m b a . i r </a>
-        </div>
-
-        <a href="tel:02146013418" class="row footer-call">
-            <div class="col-4">
-                <img src="http://127.0.0.1:8000/img/icons/tel-blue.png" width="20px" alt="">
+<footer class="footer-wrapper">
+    <div class="container">
+        <div class="footer">
+            <div class="col-lg-9 col-md-9 col-sm-12">
+                <h3>{{$footer->title ?? ''}}</h3>
+                <p>{{$footer->passage ?? ''}}</p>
             </div>
-            <div class="col-8 text-center border-right">
-                تماس بگیرید
-                <span class="text-danger mr-2" dir="ltr">021&nbsp;&nbsp;46013418</span>
-            </div>
-        </a>
-
-        <a href="http://t.me/jgraphic" class="row footer-call">
-            <div class="col-4">
-                <img class="mt-1" src="http://127.0.0.1:8000/img/icons/telegram-blue.png" width="20px" alt="">
-            </div>
-            <div class="col-8 text-center border-right">
-                چت کنید
-
-            </div>
-        </a>
-
-        <hr>
-
-        <div class="address text-center mt-3">
-            <span class="text-black"> دفتر تهران : </span>
-            <br>
-            بلوار شهید اشرفی اصفهانی - بالا تر از چهار راه پونک - کوچه واحدی (هفتم) - پلاک 64 - طبفه 6 - واحد 18
-        </div>
-
-        <hr>
-
-        <div class="text-black text-center koodak my-3">
-            <a class="text-black" href="tel:02146013356">021&nbsp;46013356</a>
-            <br>
-            <a class="text-black" href="tel:02146013418">021&nbsp;46013418</a>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6">
-                <div class="row social-icons">
-                    <div class="col-4">
-                        <a href="http://t.me/royanrasaneh">
-                            <img src="http://127.0.0.1:8000/img/icons/telegram-white.png" class="mt-2" width="25px">
-                        </a>
-                    </div>
-                    <div class="col-4 center">
-                        <a href="http://instagram.com/alizohrabi">
-                            <img src="http://127.0.0.1:8000/img/icons/insta-white.png" class="mt-2" width="25px">
-                        </a>
-                    </div>
-                    <div class="col-4">
-                        <a href="http://www.rqmedia.ir">
-                            <img src="http://127.0.0.1:8000/img/icons/globe-white.png" class="mt-2" width="25px">
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3"></div>
-        </div>
-
-    </div>
-
-    <div class="d-none d-sm-block pt-4">
-
-        <div class="row">
-
-            <div class="text-center mb-4 col-md-4">
-                <img src="http://127.0.0.1:8000/img/icons/royan.png" width="60px" height="60px" alt="رویان رسانه">
-                <p class="royan-desc text-black"> اولین موسس باشگاه مشتریان هوشمند در ایران </p>
-                <a href="http://www.rqmedia.ir" class="d-block royan-desc text-white"> w w w . m m b a . i r </a>
-            </div>
-
-            <div class="col-md-4">
-                <a href="tel:02146013418" class="row footer-call">
-                    <div class="col-4">
-                        <img src="http://127.0.0.1:8000/img/icons/tel-blue.png" width="20px" alt="">
-                    </div>
-                    <div class="col-8 text-center border-right">
-                        تماس بگیرید
-                        <span class="text-danger mr-2" dir="ltr">021&nbsp;&nbsp;46013418</span>
-                    </div>
-                </a>
-
-                <a href="http://t.me/jgraphic" class="row footer-call">
-                    <div class="col-4">
-                        <img class="mt-1" src="http://127.0.0.1:8000/img/icons/telegram-blue.png" width="20px" alt="">
-                    </div>
-                    <div class="col-8 text-center border-right">
-                        چت کنید
-
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-4 address pt-2">
-                <span class="text-black"> دفتر تهران : </span>
-                <br>
-                بلوار شهید اشرفی اصفهانی - بالا تر از چهار راه پونک - کوچه واحدی (هفتم) - پلاک 64 - طبفه 6 - واحد 18
-                <br>
-                <a class="text-black koodak mx-4" href="tel:02146013356">021&nbsp;46013356</a>
-                <a class="text-black koodak mx-4" href="tel:02146013418">021&nbsp;46013418</a>
+            <div class="col-lg-3 col-md-3 col-sm-12">
+                <h3> {{$footer->links_title}} </h3>
+                <ul>
+                    @if (isset($footer->links) && count($footer->links))
+                        @foreach ($footer->links as $key => $link)
+                            <li><a href="{{$link->href}}">{{$link->name}}</a></li>
+                        @endforeach
+                    @endif
+                </ul>
             </div>
         </div>
-
-        <hr class="pb-3 m-0">
-
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6">
-                <div class="row social-icons">
-                    <div class="col-4">
-                        <a href="http://t.me/royanrasaneh">
-                            <img src="http://127.0.0.1:8000/img/icons/telegram-white.png" class="mt-2" width="25px">
-                        </a>
-                    </div>
-                    <div class="col-4 center">
-                        <a href="http://instagram.com/alizohrabi">
-                            <img src="http://127.0.0.1:8000/img/icons/insta-white.png" class="mt-2" width="25px">
-                        </a>
-                    </div>
-                    <div class="col-4">
-                        <a href="http://www.rqmedia.ir">
-                            <img src="http://127.0.0.1:8000/img/icons/globe-white.png" class="mt-2" width="25px">
-                        </a>
-                    </div>
-                </div>
+        <div class="copyright">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-xs-12">
+                <p class="copy">&copy; {{$footer->copy_right ?? ''}}</p>
             </div>
-            <div class="col-3"></div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-xs-12">
+                <p class="royan">{{$footer->quote}}</p>
+            </div>
         </div>
     </div>
 </footer>
-<style media="screen">
-
-body{
-    overflow-x: hidden;
-}
-
-@font-face { font-family: 'Calibri'; src: url('../fonts/Calibri.ttf'); }
-@font-face { font-family: 'Yekan'; src: url('../fonts/Yekan.woff'); }
-@font-face { font-family: 'Dinar'; src: url('../fonts/Dinar.ttf'); }
-@font-face { font-family: 'Yekanp'; src: url('../fonts/Yekanp.ttf'); }
-@font-face { font-family: 'Traf'; src: url('../fonts/Traf.ttf'); }
-@font-face { font-family: 'Koodak'; src: url('../fonts/Koodak.ttf');}
-@font-face { font-family: 'Entezar'; src: url('../fonts/Entezar.ttf'); }
-footer{
-    padding: 10px;
-    color: white;
-}
-
-footer div {
-    text-align: center;
-}
-
-.royan-desc {
-    font-size: 0.9em;
-    margin: 0;
-}
-
-.footer-call {
-    color: #00B9CF;
-    padding: 10px 0px;
-    font-size: 1.2em;
-    font-family: 'Yekan';
-    background-color: white;
-    margin: 5px 0px 0px 10px;
-}
-.footer-call:hover {
-    color: #00B9CF;
-}
-
-.border-right {
-    border-right: 2px solid #00B9CF;
-}
-
-.royanrasaneh-telegram {
-    font-size: 0.8em;
-    font-family: 'Calibri';
-}
-
-.address {
-    font-size: 0.9em;
-}
-
-.social-icons div.center {
-    border-right: 1px solid white;
-    border-left: 1px solid white;
-}
-.social-icons img {
-    margin-bottom: 8px;
-}
-.text-black {
-    color: black !important;
-}
-
-@media only screen and (max-width: 500px) {
-    .address {
-        font-size: 0.85em;
-    }
-}
-</style>

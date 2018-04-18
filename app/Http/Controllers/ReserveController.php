fb6603878cb53698932519a8d1a692f09ae3480c
eylay <ali.seyfi68@gmail.com>
2018-04-18 11:23:01 +0430

    Initial Commit

---
+++ app/Http/Controllers/ReserveController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reserve;
+use Illuminate\Http\Request;
+
+class ReserveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Reserve  $reserve
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reserve $reserve)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Reserve  $reserve
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reserve $reserve)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Reserve  $reserve
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reserve $reserve)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Reserve  $reserve
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reserve $reserve)
+    {
+        //
+    }
+}

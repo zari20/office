f98007abaa9cefda00137bb839724abe8184dc8b
eylay <ali.seyfi68@gmail.com>
2018-04-23 14:20:35 +0430

    new schedule form ready for calendar

--- resources/views/reserve_fragments/new_schedule.blade.php
+++ resources/views/reserve_fragments/new_schedule.blade.php
@@ -2,30 +2,30 @@
     <div class="alert alert-warning"> <h3 class="alert-heading"> رزرو سالن </h3> </div>
     <hr>
     <div class="row">
-        <div class="form-group col-md-4 styled-select slate">
-            <label for="room"> <i class="fa fa-hotel ml-1"></i> نوع اتاق <i class="fa fa-asterisk text-danger"></i></label>
+        <div class="form-group col-md-3">
+            <label for="room"> <i class="fa fa-hotel ml-1"></i> نوع اتاق </label>
             <select class="form-control" name="room_id" id="room" onchange="changeService($(this))">
                 @foreach ($rooms as $key => $room)
                     <option value="{{$room->id}}" data-cost="{{$room->cost_pre_hour}}">{{$room->name}}</option>
                 @endforeach
             </select>
         </div>
-        <div class="form-group col-md-4">
-            <label for="room-count"> <i class="fa fa-hourglass-1 ml-1"></i> تعداد ساعات <i class="fa fa-asterisk text-danger"></i></label>
-            <input type="number" step="{{$room->time_steps}}" class="form-control" id="room-count" name="hours" value="{{old('hours') ?? 1}}" required placeholder="*الزامی" onchange="changeCount($(this))" data-type="room">
+        <div class="form-group col-md-3">
+            <label for="date"> <i class="fa fa-calendar ml-1"></i> تاریخ </label>
+            <input type="text" data-calendar="persian" readonly autocomplete="off" class="form-control" id="date" name="date" value="{{old('date') ?? date_picker_date(date('Y-m-d'))}}" required>
         </div>
-        <div class="form-group col-md-4">
-            <label for="date"> <i class="fa fa-calendar ml-1"></i> تاریخ <i class="fa fa-asterisk text-danger"></i></label>
-            <input type="text" data-calendar="persian" readonly autocomplete="off" class="form-control" id="date" name="date" value="{{old('date')}}" required placeholder="*الزامی">
+        <div class="form-group col-md-3">
+            <label for="room-count"> <i class="fa fa-hourglass-1 ml-1"></i> تعداد ساعات </label>
+            <input type="number" class="form-control" id="room-count" name="hours" value="{{old('hours') ?? 0}}" disabled>
         </div>
-        <div class="form-group col-md-4">
-            <label for="time"> <i class="fa fa-clock-o ml-1"></i> ساعت <i class="fa fa-asterisk text-danger"></i></label>
-            <input type="time" class="form-control" id="time" name="time" value="{{old('time')}}" required placeholder="*الزامی" step="1800" value="13:30">
-        </div>
-        <div class="form-group col-md-4">
+        <div class="form-group col-md-3">
             <label for="room-final-cost"> <i class="fa fa-money ml-1"></i> هزینه نهایی به تومان </label>
-            <input type="text" step="5000" class="form-control" id="room-final-cost" name="room_cost" value="{{$rooms->first()->cost_pre_hour ?? 0}}" disabled>
+            <input type="text" class="form-control" id="room-final-cost" name="room_cost" value="0" disabled>
         </div>
+    </div>
+
+    <div class="">
+        
     </div>
 
     <div class="alert alert-info">

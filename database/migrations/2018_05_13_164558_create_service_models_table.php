99364aa7e2c7ebd368b8dc4a050f5f8c6f3b4006
eylay <ali.seyfi68@gmail.com>
2018-05-13 16:59:19 +0430

    migrations for dynamic services

---
+++ database/migrations/2018_05_13_164558_create_service_models_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServiceModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_models', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('service_type_id');
+            $table->string('title');
+            $table->unsignedBigInteger('base');
+            $table->boolean('countable')->default(1);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_models');
+    }
+}
